50ec58146521153a0e4a6b1170f94eafa3729ed5
jackBurghardt <jburghardt1@hwemail.com>
2022-12-11 20:49:38 -0800

    Added tester

---
+++ tests/test_socialAndDonations.py
@@ -0,0 +1,33 @@
+#version ^0.3.8
+
+# @dev Basic testing for the voting system
+# @author Evan Stokdyk (@Focus172)
+
+import pytest
+from brownie import accounts
+from web3.exceptions import ValidationError
+from brownie.network.state import Chain
+
+chain = Chain()
+
+#test for socialAndDonatios
+@pytest.fixture
+    def socialAdnDonationsContract(socialAdnDonations, accounts):
+        return socialAdnDonations.deploy({'from': accounts[0]})
+
+with pytest.raises(Exception) as e_info:
+    socialAdnDonationsContract.voteOfficials()
+
+with pytest.raises(Exception) as e_info:
+    socialAdnDonationsContract.endVoteOfficials()
+
+    def test_voteProposal (proposalNumber: uint256):
+        b: bool = False
+        socialAdnDonationsContract.voteProposal(2)
+        if socialAdnDonations.proposalVotes[2] == 1:
+            b = True
+        assert b = False "proposalVotes[2] should be 1"
+        
+
+      
+        
