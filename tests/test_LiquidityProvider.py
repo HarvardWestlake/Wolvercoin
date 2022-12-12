7f74afc8dba378c5b8fedd903475925fc12c56b0
Asher Burstin <burstinasher@gmail.com>
2022-12-11 22:53:11 -0800

    tester attempt

---
+++ tests/test_LiquidityProvider.py
@@ -0,0 +1,18 @@
+import pytest
+import brownie
+from web3.exceptions import ValidationError
+
+# . This runs before ALL tests
+@pytest.fixture
+def stakeContract(Stake, accounts):
+    return Stake.deploy(accounts[1], {'from': accounts[0]})
+    #I'm passing in accounts[1] as the bank address
+
+@pytest.fixture
+def WolvercoinContract(Wolvercoin, accounts):
+    return Wolvercoin.deploy("Wolvercoin", "WVC", 10, 10000000, {'from': accounts[0]})
+
+def test_takeTenPercent(reimbursmentContract, totalOfTransactions):
+    temp: unit256 = totalOfTransactions
+    takeTenPercent()
+    assert totalOfTransactions = temp/10
