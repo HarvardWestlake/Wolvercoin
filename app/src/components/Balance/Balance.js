f8e85f70ca75c9ac1af1038c9fa422947836663b
Andrew Theiss <andrew.theiss@gmail.com>
2022-12-12 23:48:08 -0800

    Prepare for Web3 integration

--- app/src/components/Balance/Balance.js
+++ app/src/components/Balance/Balance.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Web3 from "../Web3/Web3.js"
 
 class Balance extends React.Component {
     render() {
-      return (<div className="balance">Balance</div>)
+      return (<div className="balance">Balance
+      <Web3 />
+      </div>)
     }
 };
 
---
+++ app/src/components/Web3/Web3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Web3 extends React.Component {
+    render() {
+      return (<div className="footer">
+        <div>Web3</div>
+      </div>)
+    }
+};
+
+export default Web3;
