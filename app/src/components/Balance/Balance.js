8967978cdfbebd029e191986a40a1ab30cbaddbd
Andrew Theiss <andrew.theiss@gmail.com>
2022-12-13 02:30:40 -0800

    Update metamask to connect client

--- app/src/components/Balance/Balance.js
+++ app/src/components/Balance/Balance.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Web3 from "../Web3/Web3.js"
 
 class Balance extends React.Component {
     render() {
       return (<div className="balance">Balance
-      <Web3 />
       </div>)
     }
 };
