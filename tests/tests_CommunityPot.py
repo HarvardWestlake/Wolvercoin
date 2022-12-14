1c1b73ec555b41b49c8bd329de7ebca34786fd78
Konnie Duan <kddypublic84@yahoo.com>
2022-12-14 14:55:16 -0800

    tests saved

---
+++ tests/tests_CommunityPot.py
@@ -0,0 +1,26 @@
+#version ^0.3.8
+import pytest
+from brownie import accounts
+from web3.exceptions import ValidationError
+
+@pytest.fixture
+def communityPotContract (CommunityPot, accounts):
+    return CommunityPot.deploy({'from': accounts[0]})
+
+def  _as_wei_value(base, conversion):
+    if conversion == "wei":
+        return base
+    if conversion == "gwei":
+        return base * (10 ** 9)
+    return base * (10 ** 18)
+
+def test_addMoney(communityPotContract, accounts):
+    assert communityPotContract.getMoney()==0
+    communityPotContract.addMoney (10)
+    assert communityPotContract.getMoney()==10
+
+def test_setElectedOfficials (communityPotContract, accounts):
+    newElectedOfficials = [accounts[0],accounts[1],accounts[2]]
+    assert newElectedOfficials!=communityPotContract.getElectedOfficials()
+    communityPotContract.setElectedOfficials (newElectedOfficials)
+    assert  newElectedOfficials==communityPotContract.getElectedOfficials()
