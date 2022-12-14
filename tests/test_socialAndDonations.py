74fd75c08f8201aa5752b6818b5af058c804238a
jackBurghardt <jburghardt1@hwemail.com>
2022-12-14 14:31:03 -0800

    Created Social and Donations File

--- tests/test_socialAndDonations.py
+++ tests/test_socialAndDonations.py
@@ -11,26 +11,26 @@ chain = Chain()
 
 #test for socialAndDonatios
 @pytest.fixture
-def socialAdnDonationsContract(socialAdnDonations, accounts):
-    return socialAdnDonations.deploy("0x0000000000000000000000000000000000000000" , {'from': accounts[0]})
+def socialAndDonationsContract(socialAndDonations, accounts):
+    return socialAndDonations.deploy("0x0000000000000000000000000000000000000000" , {'from': accounts[0]})
 
 with pytest.raises(Exception) as e_info:
-    socialAdnDonationsContract.voteOfficials()
+    socialAndDonationsContract.voteOfficials()
 
 with pytest.raises(Exception) as e_info:
-    socialAdnDonationsContract.endVoteOfficials()
+    socialAndDonationsContract.endVoteOfficials()
 
-def test_voteProposal (socialAdnDonationsContract):
-   one = socialAdnDonationsContract.getProposalVotes(2)
-   socialAdnDonationsContract.voteProposal(2)
-   two = socialAdnDonationsContract.getProposalVotes(2)
+def test_voteProposal (socialAndDonationsContract):
+   one = socialAndDonationsContract.getProposalVotes(2)
+   socialAndDonationsContract.voteProposal(2)
+   two = socialAndDonationsContract.getProposalVotes(2)
    assert one != two
     
 
 #def test_donate():
     #b: bool = False
     #balance = accounts[1].balance()
-    #socialAdnDonationsContract.donate(accounts[1].address, accounts[0].balance)
+    #socialAndDonationsContract.donate(accounts[1].address, accounts[0].balance)
    # 'accounts[0].transfer(accounts[1], "10 ether", gas_price=0)'
     #if(accounts[0].balance == 0):
        # b == True
