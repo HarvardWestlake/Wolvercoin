69f81950408ca372c0fb78ef1b56c5b04595eb78
Andrew Theiss <andrew.theiss@gmail.com>
2022-12-13 16:00:24 -0800

    Adjust context for next example to try

--- app/src/components/Web3/MetaMask.js
+++ app/src/components/Web3/MetaMask.js
@@ -30,22 +30,16 @@ class MetaMask extends React.Component {
     return scanAddress;
   }
 
-  async disconnectFromMetamask() {
+  async update() {
 
-  }
-
-  async connectToMetamask() {
-    //this.props.web3.connectAccount();
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const accounts = await provider.send("eth_requestAccounts", []);
-
-
     const wolvercoinContract = new ethers.Contract(
       Contracts.CONTRACTS.wolvercoin.address, 
       Contracts.CONTRACTS.wolvercoin.ABI.abi, 
       provider
     );
-    const chosenAccount = accounts[0];
+    const chosenAccount = this.props.web3Context.connectedAccount;
     const tokenName = await wolvercoinContract.name();
     const tokenBalance = await wolvercoinContract.balanceOf(chosenAccount);
     const tokenUnits = await wolvercoinContract.decimals();
@@ -55,8 +49,20 @@ class MetaMask extends React.Component {
     this.setState({ selectedAddress: chosenAccount, tokenName, tokenBalanceInEther, abreviatedWallet, scanAddress })
  
   }
+
+  async disconnectFromMetamask() {
+
+  }
+
+  async connectToMetamask() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const accounts = await provider.send("eth_requestAccounts", []);
+    await this.props.web3Context.setConnectedAccount(accounts[0]);
+    this.props.web3Context.connectedAccount = accounts[0];
+    this.setState({accounts : accounts[0]});
+  }
   renderMetamask() {
-    if (!this.state.selectedAddress) {
+    if (!this.props.web3Context.connectedAccount) {
       return (
         <div className="gas-station-container">
         <span>
