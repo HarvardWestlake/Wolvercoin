c2794818939dbb00aaf0f1ee48bc3f227698d17f
Andrew <andrewgtz099@gmail.com>
2022-12-16 14:48:23 -0800

    Working on tests

--- tests/test_socialAndDonations.py
+++ tests/test_socialAndDonations.py
@@ -11,36 +11,27 @@ chain = Chain()
 
 #test for socialAndDonatios
 @pytest.fixture
-def socialAdnDonationsContract(socialAdnDonations, accounts):
+def socialAdnDonationsContract(socialAdnDonations, accounts): 
     return socialAdnDonations.deploy("0x0000000000000000000000000000000000000000" , {'from': accounts[0]})
 
-with pytest.raises(Exception) as e_info:
-    socialAdnDonationsContract.voteOfficials()
+def test_voteOfficials (socialAdnDonationsContract):
+    with pytest.raises(Exception) as e_info:
+        socialAdnDonationsContract.voteOfficials()
 
-with pytest.raises(Exception) as e_info:
-    socialAdnDonationsContract.endVoteOfficials()
+    with pytest.raises(Exception) as e_info:
+        socialAdnDonationsContract.endVoteOfficials()
 
-    def test_donate():
-        b: bool = False
-        balance = accounts[0].balance()
-        socialAdnDonationsContract.donate(accounts[0].address,accounts[1].address,balance)
-        if(accounts[0].balance == 0):
-            b == True
-        assert b == True
+def test_donate(socialAdnDonationsContract, accounts):
+    b: bool = False
+    balance = accounts[0].balance()
+    socialAdnDonationsContract.donate(accounts[0],accounts[1],balance, {'from' : accounts[0]})
+    if(accounts[0].balance == 0):
+        b == True
+    assert b == True
+"""
 def test_voteProposal (socialAdnDonationsContract):
    one = socialAdnDonationsContract.getProposalVotes(2)
    socialAdnDonationsContract.voteProposal(2)
    two = socialAdnDonationsContract.getProposalVotes(2)
    assert one != two
-    
-
-    
-def test_donate():
-    balance = accounts[0].balance()
-    accounts[0].transfer(accounts[1], "10 ether", gas_price=0)
-    assert balance - "10 ether" == accounts[0].balance()    
-    """
-
-    
-    
->>>>>>> code_socialAndDonations
+"""
