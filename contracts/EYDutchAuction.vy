2a809141abc8133de9f865486dbd71b4f1d90670
Eric Yoon <emjyoon@gmail.com>
2023-01-05 21:06:44 -0800

    Finish dutch auctions

--- contracts/EYDutchAuction.vy
+++ contracts/EYDutchAuction.vy
@@ -17,9 +17,10 @@ struct AuctionItem:
     endPrice: uint256
     startDate: uint256
     endDate: uint256
-    nftTokenId: address
+    nftTokenId: uint256
 
 auctionItems: public(HashMap[String[50], AuctionItem])
+auctionItemsArr: public(DynArray[String[50], 100]) # A list of the names of all the auction items currently active
 erc20: ERC20WithAdminAccess
 erc721: ERC721WithAdminAccess
 
@@ -28,17 +29,26 @@ def __init__(erc20address: address, erc721address: address):
     self.erc20 = ERC20WithAdminAccess(erc20address)
     self.erc721 = ERC721WithAdminAccess(erc721address)
 
+@internal
+def findIndexOfItemInItemsArr(name: String[50]) -> int256:
+    for i in range(100):
+        if self.auctionItemsArr[i] == name:
+            return i
+    return -1
 
 # To call this function, one must approve the transfer via 721
 @external
-def createAuctionItem(name: String[50], startPrice: uint256, endPrice: uint256, startDate: uint256, endDate: uint256, nftTokenId: address):
+def createAuctionItem(name: String[50], startPrice: uint256, endPrice: uint256, startDate: uint256, endDate: uint256, nftTokenId: uint256):
     assert name != ""
     assert endPrice > 0
     assert startPrice > endPrice
-    assert startDate > block.timestamp
+    assert startDate >= block.timestamp
     assert endDate > startDate
     assert self.erc721.ownerOf(nftTokenId) == msg.sender
 
+    # Move the NFT to the property of this contract for safekeeping
+    self.erc721.transferFrom(msg.sender, self, nftTokenId)
+    
     self.auctionItems[name] = AuctionItem({
         name: name,
         seller: msg.sender,
@@ -48,25 +58,28 @@ def createAuctionItem(name: String[50], startPrice: uint256, endPrice: uint256, 
         endDate: endDate,
         nftTokenId: nftTokenId
     })
-    # Move the NFT to the property of this contract for safekeeping
-    self.erc721._transferFrom(msg.sender, self, nftTokenId)
+    self.auctionItemsArr.append(name)
 
-@external
-def getPrice(name: String[50]) -> uint256:
+@internal
+def _getPrice(name: String[50]) -> uint256:
     assert name != ""
     auctionItem: AuctionItem = self.auctionItems[name]
     assert auctionItem.name == name
-    assert msg.timestamp > auctionItem.startDate
+    assert block.timestamp >= auctionItem.startDate
     
-    if msg.timestamp > auctionItem.endDate:
+    if block.timestamp >= auctionItem.endDate:
         return auctionItem.endPrice
 
     # Linearly interpolate to find the price
-    timeSinceStart: uint256 = msg.timestamp - auctionItem.startDate
+    timeSinceStart: uint256 = block.timestamp - auctionItem.startDate
     totalDuration: uint256 = auctionItem.endDate - auctionItem.startDate
     priceRange: uint256 = auctionItem.startPrice - auctionItem.endPrice
-    progress: uint256 = timeSinceStart / totalDuration
-    return auctionItem.endPrice + priceRange * progress
+    progress: decimal = convert(timeSinceStart, decimal) / convert(totalDuration, decimal)
+    return convert(convert(auctionItem.startPrice, decimal) - convert(priceRange, decimal) * progress, uint256)
+
+@external
+def getPrice(name: String[50]) -> uint256:
+    return self._getPrice(name)
 
 # To call this function, one must approve the monetary transfer via 20
 @external
@@ -75,13 +88,22 @@ def buy(name: String[50]):
     auctionItem: AuctionItem = self.auctionItems[name]
     assert auctionItem.name == name
 
-    assert msg.timestamp > auctionItem.startDate
-    price: uint256 = self.getPrice(name)
+    assert block.timestamp > auctionItem.startDate
+    price: uint256 = self._getPrice(name)
     assert self.erc20.getBalanceOf(msg.sender) >= price
 
     self.erc20.transferFrom(msg.sender, auctionItem.seller, price)
     self.erc721.transferFrom(self, msg.sender, auctionItem.nftTokenId)
 
+    self.auctionItems[name] = empty(AuctionItem)
+    i: int256 = self.findIndexOfItemInItemsArr(name)
+    if i != -1:
+        self.auctionItemsArr[i] = self.auctionItemsArr[len(self.auctionItemsArr) - 1] # Make the last element take the one you want to remove's place...
+        self.auctionItemsArr.pop() # ...and then remove the last element
+
+@external
+def getActiveAuctionItems() -> DynArray[String[50], 100]:
+    return self.auctionItemsArr
 
 #region Trivial getters
 @external
@@ -115,7 +137,7 @@ def getEndPrice(name: String[50]) -> uint256:
     assert auctionItem.name == name
     return auctionItem.endPrice
 @external
-def getNFT(name: String[50]) -> address:
+def getNFT(name: String[50]) -> uint256:
     assert name != ""
     auctionItem: AuctionItem = self.auctionItems[name]
     assert auctionItem.name == name
--- tests/test_eyDutchAuction.py
+++ tests/test_eyDutchAuction.py
@@ -1,6 +1,7 @@
 #version ^0.3.8
 import pytest
 import brownie
+from brownie import chain
 
 DEFAULT_GAS = 100000
 
@@ -17,15 +18,129 @@ def erc20Contract(Token, accounts):
 @pytest.fixture
 def erc721Contract(NFT, accounts):
     return NFT.deploy(
-        "TopT", # password
+        12345, # password
         {'from': accounts[0]}
     )
 
 @pytest.fixture
-def DutchAuctionContract(EYDutchAuction, erc20Contract, erc721Contract, accounts):
+def dutchAuctionContract(EYDutchAuction, erc20Contract, erc721Contract, accounts):
     return EYDutchAuction.deploy(
         erc20Contract,
         erc721Contract,
         {'from': accounts[0]}
     )
 
+def test_create_auction_item(dutchAuctionContract, erc20Contract, erc721Contract, accounts):
+    admin = accounts[0]
+    creator = accounts[1]
+
+    mintResult = erc721Contract.mint(creator, "https://example.com?doubledate", {'from': admin})
+    mintedTokenId = mintResult.events["Transfer"]["tokenId"]
+    assert erc721Contract.approve(dutchAuctionContract, mintedTokenId, {'from': creator})
+    assert dutchAuctionContract.createAuctionItem(
+        "Double Date with Theiss and Sassie", # Name
+        25, # Start price
+        5, # End price
+        chain.time() + 10000, # Start time
+        chain.time() + 20000, # End time
+        mintedTokenId, # Token ID
+        {'from': creator}
+    )
+
+def test_create_auction_item(dutchAuctionContract, erc20Contract, erc721Contract, accounts):
+    admin = accounts[0]
+    creator = accounts[1]
+
+    mintResult = erc721Contract.mint(creator, "https://example.com?doubledate", {'from': admin})
+    mintedTokenId = mintResult.events["Transfer"]["tokenId"]
+    assert erc721Contract.approve(dutchAuctionContract, mintedTokenId, {'from': creator})
+    assert dutchAuctionContract.createAuctionItem(
+        "Double Date with Theiss and Sassie", # Name
+        25, # Start price
+        5, # End price
+        chain.time() + 10000, # Start time
+        chain.time() + 20000, # End time
+        mintedTokenId, # Token ID
+        {'from': creator}
+    )
+
+def test_getters(dutchAuctionContract, erc20Contract, erc721Contract, accounts):
+    admin = accounts[0]
+    creator = accounts[1]
+
+    mintResult = erc721Contract.mint(creator, "https://example.com?fieldtrip", {'from': admin})
+    mintedTokenId = mintResult.events["Transfer"]["tokenId"]
+    assert erc721Contract.approve(dutchAuctionContract, mintedTokenId, {'from': creator})
+    startTime = chain.time() + 10000
+    endTime = chain.time() + 20000
+    assert dutchAuctionContract.createAuctionItem(
+        "Field Trip to Bora Bora", # Name
+        25, # Start price
+        5, # End price
+        startTime, # Start time
+        endTime, # End time
+        mintedTokenId, # Token ID
+        {'from': creator}
+    )
+    assert dutchAuctionContract.getActiveAuctionItems({'from': creator}).return_value == ["Field Trip to Bora Bora"]
+    assert dutchAuctionContract.getSeller("Field Trip to Bora Bora", {'from': creator}).return_value == creator
+    assert dutchAuctionContract.getStartDate("Field Trip to Bora Bora", {'from': creator}).return_value == startTime
+    assert dutchAuctionContract.getEndDate("Field Trip to Bora Bora", {'from': creator}).return_value == endTime
+    assert dutchAuctionContract.getStartPrice("Field Trip to Bora Bora", {'from': creator}).return_value == 25
+    assert dutchAuctionContract.getEndPrice("Field Trip to Bora Bora", {'from': creator}).return_value == 5
+    assert dutchAuctionContract.getNFT("Field Trip to Bora Bora", {'from': creator}).return_value == mintedTokenId
+
+def test_get_price(dutchAuctionContract, erc20Contract, erc721Contract, accounts):
+    admin = accounts[0]
+    creator = accounts[1]
+
+    mintResult = erc721Contract.mint(creator, "https://example.com?chocolate", {'from': admin})
+    mintedTokenId = mintResult.events["Transfer"]["tokenId"]
+    assert erc721Contract.approve(dutchAuctionContract, mintedTokenId, {'from': creator})
+    assert dutchAuctionContract.createAuctionItem(
+        "Chocolate Tasting", # Name
+        420, # Start price
+        220, # End price
+        chain.time() + 10000, # Start time
+        chain.time() + 20000, # End time
+        mintedTokenId, # Token ID
+        {'from': creator}
+    )
+    chain.sleep(10000)
+    assert abs(dutchAuctionContract.getPrice("Chocolate Tasting").return_value - 420) < 2 # Get difference and ABS to account for slow code
+    chain.sleep(5000)
+    assert abs(dutchAuctionContract.getPrice("Chocolate Tasting").return_value - 320) < 2
+    chain.sleep(5000)
+    assert abs(dutchAuctionContract.getPrice("Chocolate Tasting").return_value - 220) < 2
+
+def test_buy(dutchAuctionContract, erc20Contract, erc721Contract, accounts):
+    admin = accounts[0]
+    creator = accounts[1]
+    donator = accounts[2]
+
+    mintResult = erc721Contract.mint(creator, "https://example.com?smokesesh", {'from': admin})
+    mintedTokenId = mintResult.events["Transfer"]["tokenId"]
+    assert erc721Contract.approve(dutchAuctionContract, mintedTokenId, {'from': creator})
+    assert dutchAuctionContract.createAuctionItem(
+        "Smoke Sesh with Top T", # Name
+        420, # Start price
+        220, # End price
+        chain.time() + 10000, # Start time
+        chain.time() + 20000, # End time
+        mintedTokenId, # Token ID
+        {'from': creator}
+    )
+    chain.sleep(15000)
+    price = dutchAuctionContract.getPrice("Smoke Sesh with Top T").return_value
+
+    assert erc20Contract.mint(donator, 69420, {'from': admin}) # Supply the account with some token
+    assert str(erc20Contract.getBalanceOf(donator)) == "69420"
+    assert erc20Contract.approve(dutchAuctionContract.address, 69420, {'from': donator}) # Approve expenditure
+    assert str(erc20Contract.getApprovedAmountOf(donator, dutchAuctionContract.address).return_value) == "69420"
+
+    assert dutchAuctionContract.buy("Smoke Sesh with Top T", {'from': donator})
+
+    newBalance = int(erc20Contract.getBalanceOf(donator))
+    assert abs(69420 - newBalance - price) < 2
+
+    assert erc721Contract.ownerOf(mintedTokenId) == donator
