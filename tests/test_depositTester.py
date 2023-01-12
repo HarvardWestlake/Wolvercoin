86c95a59cf083e8206c70250ed504239215909fa
Matthew <matthewk8819@gmail.com>
2023-01-12 09:11:36 -0800

    ROFLCOPTER

--- tests/test_depositTester.py
+++ tests/test_depositTester.py
@@ -7,12 +7,12 @@ from brownie.network.state import Chain
 
 chain = Chain()
 
-@pytest.fixture
-#def test_take10Percent()
-def test_deposit10Percent(tester,testProvider):
-    preBank=tester.balanceOf[tester.bank]
-    preProvider=tester.balanceOf[testProvider]
-    tester.deposit10Percent(testProvider)    
-    postBank=tester.balanceOf[tester.bank]
-    postProvider=tester.balanceOf[testProvider]
-    assert postBank==preBank-tester.totalOfTransactions*.1 and postProvider==preProvider+tester.totalOfTransactions*.1, "successfully invested 10%"
+# TODO: make a pre-run function initializing your contract using @pytest.fixture
+
+# def test_deposit10Percent(tester,testProvider):
+#     preBank=tester.balanceOf[tester.bank]
+#     preProvider=tester.balanceOf[testProvider]
+#     tester.deposit10Percent(testProvider)    
+#     postBank=tester.balanceOf[tester.bank]
+#     postProvider=tester.balanceOf[testProvider]
+#     assert postBank==preBank-tester.totalOfTransactions*.1 and postProvider==preProvider+tester.totalOfTransactions*.1, "successfully invested 10%"
