e1be9d8df7d14abe53e6a825250f1e9273a5226b
Evan Stokdyk <evan.stokdyk@gmail.com>
2023-01-15 23:23:12 -0800

    Update Community Pot

--- contracts/CommunityPot.vy
+++ contracts/CommunityPot.vy
@@ -1,56 +1,50 @@
 # @version ^0.3.7
-interface Token:
-    def transferFrom(_from : address, _to : address, _value : uint256) -> bool: view
+
+interface ERC20:
+    def transfer(_to : address, _value : uint256) -> bool: nonpayable
     def getBalanceOf(_user: address) -> uint256: view
-TokenContract: public(Token)
 
+interface ActiveUser:
+    def getIsAdmin(potentialAdmin: address) -> bool: view
 
-electedOfficials: public(DynArray [address, 3])
-moneyStored: public(uint256)
-PotAddress: public(address)
+tokenContractAddress: ERC20
+activeUserContract: ActiveUser
+
+# there is a bit of strange logic for an array over a map but here is my best logic
+# > for small values arrays can be faster (see vectors in c++)
+# > size is always fixed so lookup times are consistant
+# > doesn't add space when checking bad input
+electedOfficials: public(DynArray[address, 8])
 
 @external
-def __init__(thePotAddress: address, ERC20address: address):
-    self.moneyStored = 0
+def __init__(tokenContractAddress: address, activeUserContract: address):
     self.electedOfficials = []
-    self.PotAddress=thePotAddress
-    self.TokenContract=Token(ERC20address)
-
+    self.tokenContractAddress = ERC20(tokenContractAddress)
+    self.activeUserContract = ActiveUser(activeUserContract)
 
 @external
-def addMoney (amount: uint256):
-    self.moneyStored = self.moneyStored + amount
+def getMoneyStored() -> uint256:
+    return self.tokenContractAddress.getBalanceOf(self)
+
+@external
+def transact(amount: uint256, userToSendTo: address):
+    assert self.tokenContractAddress.getBalanceOf(self) >= amount
+    assert self._isAdmin(msg.sender) or self.activeUserContract.getIsAdmin(msg.sender), "Only qualified users can add an admin"
+    self.tokenContractAddress.transfer(userToSendTo, amount)
+
+@external
+def isAdmin(addressToCheck: address) -> bool:
+    return self._isAdmin(addressToCheck)
 
 @internal
-def _removeMoney (amount: uint256):
-    self.moneyStored = self.moneyStored - amount
-
-@external
-def setElectedOfficials (newEleectedOfficials: DynArray [address,3]):
-    self.electedOfficials = newEleectedOfficials
-
-@view
-@external
-def getMoney() -> uint256: 
-    return self.moneyStored
-
-@view
-@external
-def getElectedOfficials() -> DynArray[address, 3]:
-    return self.electedOfficials
-
-@external
-def Transact(amount: uint256, destAddress: address):
-    assert self.TokenContract.getBalanceOf(self.PotAddress) >= amount
-    self.TokenContract.transferFrom(self.PotAddress, destAddress, amount)
-    self._removeMoney(amount)
-
-#whether or not an address is in admin array
-@view
-@external
-def VerifyElectedOfficial(wolvAddress: address) -> bool:
-    for item in self.electedOfficials:
-        if item==wolvAddress:
+def _isAdmin(addressToCheck: address) -> bool:
+    for entry in self.electedOfficials:
+        if entry == addressToCheck:
             return True
     return False
 
+@external
+def addElectedOfficial(newElectedOfficial: address):
+    assert not self._isAdmin(newElectedOfficial), "You can't add an admin twice"
+    assert self._isAdmin(msg.sender) or self.activeUserContract.getIsAdmin(msg.sender), "Only qualified users can add an admin"
+    self.electedOfficials.append(newElectedOfficial)
--- tests/test_CommunityPot.py
+++ tests/test_CommunityPot.py
@@ -1,35 +1,54 @@
 #version ^0.3.8
 import pytest
 from brownie import accounts
-from web3.exceptions import ValidationError
-
-MONEY=500
 
 @pytest.fixture
-def communityPotContract(CommunityPot, accounts):
-    return CommunityPot.deploy(accounts[1], accounts[2], {'from': accounts[0]})
+def contractAddresses(CommunityPot, ActiveUser, Token, accounts):
+    activeUserContract = ActiveUser.deploy(accounts[1], {'from': accounts[0]})
+    tokenContract = Token.deploy("Wolvercoin", "WVC", 18, 2000, {'from': accounts[0]})
+    communityPotContract = CommunityPot.deploy(tokenContract, activeUserContract, {'from': accounts[0]})
+    return [communityPotContract, tokenContract, activeUserContract]
 
-def  _as_wei_value(base, conversion):
-    if conversion == "wei":
-        return base
-    if conversion == "gwei":
-        return base * (10 ** 9)
-    return base * (10 ** 18)
+def test_getMoneyStored(contractAddresses, accounts):
+    communityPotContract = contractAddresses[0]
+    tokenContract = contractAddresses[1]
+    _ = contractAddresses[2]
 
-def test_addMoney(communityPotContract):
-    assert communityPotContract.getMoney() == 0, "initialize money to zero"
-    communityPotContract.addMoney (10)
-    assert communityPotContract.getMoney()==10
+    assert communityPotContract.getMoneyStored().return_value == 0, "Initialize money to zero"
 
-def test_setElectedOfficials (communityPotContract, accounts):
-    newElectedOfficials =  [accounts[0],accounts[1],accounts[2]]
-    assert newElectedOfficials!=communityPotContract.getElectedOfficials()
-    communityPotContract.setElectedOfficials (newElectedOfficials)
-    assert  newElectedOfficials==communityPotContract.getElectedOfficials()
+    # accounts[0] is the minter of the token address
+    tokenContract.transfer(communityPotContract, 10, {'from': accounts[0]})
+    assert communityPotContract.getMoneyStored().return_value == 10, "Donated money should be reflected in pot"
 
-def test_VerifyElectedOfficial(communityPotContract, accounts):
-     assert communityPotContract.VerifyElectedOfficial(accounts[2])==False
+def test_addElectedOfficial(contractAddresses, accounts):
+    communityPotContract = contractAddresses[0]
+    tokenContract = contractAddresses[1]
+    _ = contractAddresses[2]
 
-def test_Transact(communityPotContract, accounts):
-    with pytest.raises(Exception) as e_info:
-        communityPotContract.Transact(MONEY, accounts[1])
+    # current officials: []
+    assert communityPotContract.isAdmin(accounts[8]).return_value == False, "Admins should not contain every user"
+
+    # owner of active user contract is accounts[0]
+    communityPotContract.addElectedOfficial(accounts[7], {'from': accounts[0]})
+    assert communityPotContract.isAdmin(accounts[7]).return_value == True, "Should be able to add admin"
+    assert communityPotContract.isAdmin(accounts[8]).return_value == False, "Adding an admin shouldn't change others status"
+
+def test_transact(contractAddresses, accounts):
+    communityPotContract = contractAddresses[0]
+    tokenContract = contractAddresses[1]
+    _ = contractAddresses[2]
+
+    initalAccountBalence = tokenContract.getBalanceOf(accounts[3])
+
+    tokenContract.transfer(communityPotContract, 10, {'from': accounts[0]})
+    communityPotContract.addElectedOfficial(accounts[5], {'from': accounts[0]})
+
+    communityPotContract.transact(2, accounts[3], {'from': accounts[5]})
+    assert tokenContract.getBalanceOf(accounts[3]) == initalAccountBalence + 2
+
+    try:
+        stopBadtransfer = False
+        communityPotContract.transfer(4, accounts[0], {'from': accounts[9]})
+    except:
+        stopBadtransfer = True
+    assert stopBadtransfer, "contract should stop non admins from transacting"
