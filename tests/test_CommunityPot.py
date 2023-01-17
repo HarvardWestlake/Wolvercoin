e8c5dda23517cca696ef567389181910a3afeccf
Casey Landecker <caseylandecker@gmail.com>
2023-01-17 14:58:52 -0800

    More cohesive VerifyElectedOfficialsTest

--- tests/test_CommunityPot.py
+++ tests/test_CommunityPot.py
@@ -46,21 +46,7 @@ def test_VerifyElectedOfficial(communityPotContract, accounts):
     assert communityPotContract.VerifyElectedOfficial(accounts[0])==True
     assert communityPotContract.VerifyElectedOfficial(accounts[1])==True
     assert communityPotContract.VerifyElectedOfficial(accounts[2])==True
-    assert communityPotContract.VerifyElectedOfficial(accounts[5])==False #edited
-
-def test_WorkingTransact(communityPotContract, erc20Contract, accounts):
-    erc20Contract.transfer(communityPotContract.getPotAddress(), 50, {'from': accounts[0]}) #idk if it is correct address 
-    communityPotContract.addMoney(50) #fairly certain addMoney isn't doing what it should
-    assert str(erc20Contract.getBalanceOf(communityPotContract.getPotAddress())) == "50"
-
-    #idk idk idk
-    senderAccount=communityPotContract.getPotAddress()
-    erc20Contract.approve(accounts[2], 25, {"from": senderAccount})
-    communityPotContract.Transact(25, accounts[2])
-
-    assert communityPotContract.getMoney()==25
-    assert str(erc20Contract.getBalanceOf(accounts[2])) == "25" #this assumes each account starts w/ 0 money
-    
+    assert communityPotContract.VerifyElectedOfficial(accounts[5])==False #edited 
       
 def test_NotEnoughToTransact(communityPotContract, accounts):
     with pytest.raises(Exception) as e_info:
