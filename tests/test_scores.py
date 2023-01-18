9879e8a320759d8a6cbd95f80258a55d2b33193a
Anika Iyer <anika.sule.iyer@gmail.com>
2023-01-18 13:59:27 -0800

    Fixed Typo

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -8,4 +8,4 @@ def CodeScoreCheck(CodeScoreCheck, accounts):
 
 def test_returnScore(CodeScoreCheck):
     assert CodeScoreCheck.getHighScore().return_value == 72
-    assert CodeScoreCheck.comapreHighScores(accounts[0], 50).return_value == "You did not beat Mr. Theiss."
+    assert CodeScoreCheck.compareHighScores(accounts[0], 50).return_value == "You did not beat Mr. Theiss."
