69c12b2e0ed9fea4fe780ad6148e205b0e018079
Anika Iyer <anika.sule.iyer@gmail.com>
2023-01-18 17:49:06 -0800

    Identified Another Issue
    Something is also wrong with another part of the CompareHighScores method. Allows for winning multiple times.

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -10,3 +10,4 @@ def test_returnScore(CodeScoreCheck, accounts):
     assert CodeScoreCheck.getHighScore().return_value == 72
     assert CodeScoreCheck.compareHighScores(accounts[0], 50).return_value == "You did not beat Mr. Theiss."
     assert CodeScoreCheck.compareHighScores(accounts[1], 75).return_value == "You beat Mr. Theiss! Epic Gamer Moment!"
+    assert CodeScoreCheck.compareHighScores(accounts[1], 75).return_value == "You cannot beat Mr. Theiss more than once."
