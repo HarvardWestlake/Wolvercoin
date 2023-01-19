91ed895fb917828d8991375076fabcefa357396f
Henry <henrydorff@gmail.com>
2023-01-19 11:43:28 -0800

    haiku test passes

--- tests/test_SubmitHaiku.py
+++ tests/test_SubmitHaiku.py
@@ -8,14 +8,16 @@ from web3.exceptions import ValidationError
 
 DEFAULT_GAS = 100000
 
+haikuSubmission = "An old silent pond\n A frog jumps into the pond—\n Splash! Silence again."
+
 # . This runs before ALL tests
 @pytest.fixture
+
 
 def submithaikuContract (SubmitHaiku, accounts):
     return SubmitHaiku.deploy (accounts[0], {'from': accounts[0]})
 
-haikuSubmission = "An old silent pond\n A frog jumps into the pond—\n Splash! Silence again."
 
-def test_canSubmitHaiku (submithaikuContract, haikuSubmission, accounts):
-    submithaikuContract.submitHaiku(accounts[0], haikuSubmission)
-    assert submithaikuContract.getHaiku (accounts[0]).equals ("An old silent pond\n A frog jumps into the pond—\n Splash! Silence again."), "Haiku in HashMap should match with the corresponding submitted haiku"
+def test_canSubmitHaiku (submithaikuContract, accounts):
+    submithaikuContract.submitHaiku(accounts[0], haikuSubmission, {'from': accounts[0]})
+    assert submithaikuContract.getHaiku (accounts[0], {'from': accounts[0]}) == "An old silent pond\n A frog jumps into the pond—\n Splash! Silence again.", "Haiku in HashMap should match with the corresponding submitted haiku"
