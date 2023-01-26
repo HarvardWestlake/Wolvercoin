d30963ba932ba12c6478f5508990e4c797a9cdbf
Eric Yoon <emjyoon@gmail.com>
2023-01-25 22:02:02 -0800

    Contract deployment and refactoring

--- app/src/components/Contexts/config.js
+++ app/src/components/Contexts/config.js
@@ -1,5 +1,9 @@
-import * as Wolvercoin from "../Web3/contracts/contracts/Token.json"
-import * as Nft from "../Web3/contracts/contracts/NFT.json"
+import * as Wolvercoin from "../Web3/contracts/Token.json"
+import * as Nft from "../Web3/contracts/NFT.json"
+import * as ActiveUser from "../Web3/contracts/ActiveUser.json"
+import * as SimpleAuction from "../Web3/contracts/SimpleAuction.json"
+import * as DutchAuction from "../Web3/contracts/DutchAuction.json"
+import * as PublicGoods from "../Web3/contracts/PublicGoods.json"
 
 export const NETWORKS = {
     GOERLI : {
@@ -18,23 +22,36 @@ export const NETWORKS = {
         chainId : 42161
     }
 }
+
+// Contracts deployed by @ericyoondotcom with account 0xdCafA75DCAED81cba4155706E9d666112950E854 on 2023-01-25 via MyEtherWallet.
+// TODO: Add a script to automatically deploy all contracts
 export const ACTIVE_CONTRACTS = {
     wolvercoin : {
-        ABI : Wolvercoin,
-        address : "0x9bced5e272c4d81b55ae092f85f6fee39545686f"
+        BrownieOutput : Wolvercoin,
+        address : "0xAfa87B1A40AdC058aAf8D86Ce81A50ed24975E94"
     },
     nft : {
-        ABI : Nft,
-        address : "0xe3D135Cc61E2f9391Fe3D678A14Cf8De6F6c9Abf"
+        BrownieOutput : Nft,
+        address : "0xaecCbC38Cb9137675e5b4Bd57FFdC0D939ac9735",
+        password : "69420"
     },
-    regularAuction : {
-        ABI : "",
-        address : "",
+    activeUser: {
+        BrownieOutput : ActiveUser,
+        address : "0x9E64ffca8a35446E9b0C015844C8B8235ed51613"
+    },
+    simpleAuction : {
+        BrownieOutput : SimpleAuction,
+        address : "0x528eE628E6d0c8BC17021b13b0F17AA1a7bD141D",
     }, 
-    dutchAction : {
-        ABI : "",
-        address : ""
+    dutchAuction : {
+        BrownieOutput : DutchAuction,
+        address : "0xd1827780098337da3fb8Fe1793405510EFb1cB35"
     },
+    publicGoods : {
+        BrownieOutput : PublicGoods,
+        address : "0x73259A179d49BA2661e172AF827CBb2751f45555"
+    },
+
     chainId: 5,
     network : NETWORKS.goerli
 };
